3d87edd07797fe6a3c66fbbfa914f1a27633e3b8
zinovevvs <ziryanov97@gmail.com>
2024-11-15 02:09:13 +0500

    Add swagger

--- app/backend/db.py
+++ app/backend/db.py
@@ -6,6 +6,16 @@ DATABASE_URL = 'sqlite:///taskmanager.db'
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
+from app.backend.db import SessionLocal
+
+
+async def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
 
 class Base(DeclarativeBase):
     pass
